5184ec96b4f753f671232e741fe4a83eb13a3484
SergeiTkachenko <fox.sergei92@gmail.com>
2022-12-31 14:36:11 +0200

    add second hw

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,2 +1,32 @@
 import Player from '@vimeo/player';
-console.log(Player);
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on(
+  'timeupdate',
+  throttle(function (currentTime) {
+    localStorage.setItem(
+      'videoplayer-current-time',
+      JSON.stringify(currentTime.seconds)
+    );
+  }, 1000)
+);
+
+player
+  .setCurrentTime(JSON.parse(localStorage.getItem('videoplayer-current-time')))
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
