f2cf47bb7c3399361348f68496f67130f5d4bf60
SergeiTkachenko <fox.sergei92@gmail.com>
2022-12-31 19:34:06 +0200

    third hw

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', throttle(onInput, 500));
+form.addEventListener('submit', onClickButton);
+
+const KEY_LOCAL_DATA = 'feedback-form-state';
+const valueLocalStorage = localStorage.getItem(KEY_LOCAL_DATA);
+const parsedLocalStorage = JSON.parse(valueLocalStorage);
+
+saveInput();
+
+function onInput(e) {
+  localStorage.setItem(
+    KEY_LOCAL_DATA,
+    JSON.stringify({
+      email: form.elements.email.value,
+      message: form.elements.message.value,
+    })
+  );
+
+  parsedLocalStorage[e.currentTarget.name] = e.currentTarget.value;
+}
+
+function saveInput() {
+  if (valueLocalStorage) {
+    form.elements.email.value = parsedLocalStorage.email || '';
+    form.elements.message.value = parsedLocalStorage.message || '';
+  }
+}
+
+function onClickButton(e) {
+  e.preventDefault();
+  e.currentTarget.reset();
+  localStorage.removeItem(KEY_LOCAL_DATA);
+  console.log(parsedLocalStorage);
+}
