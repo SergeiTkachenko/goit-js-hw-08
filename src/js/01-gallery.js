63509d4e2e1162b75b6ad17f22e1636706b0b871
SergeiTkachenko <fox.sergei92@gmail.com>
2022-12-31 09:53:27 +0200

    first hm

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,28 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
-console.log(SimpleLightbox);
+const list = document.querySelector('.gallery');
+
+const markup = galleryItems
+  .map(
+    galleryItems =>
+      `<li>
+      <a class="gallery__item" 
+      href=${galleryItems.original}>
+      <img class="gallery__image" 
+      src=${galleryItems.preview} 
+      alt=${galleryItems.description} />
+    </a>
+    </li>`
+  )
+  .join('');
+
+list.innerHTML = markup;
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
